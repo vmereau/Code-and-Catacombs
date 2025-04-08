704444ca627ab4cdeb2fa4d73638811c379681aa
Mereau Valentin <vmereau@users.noreply.github.com>
2025-04-08 10:03:13 +0200

    feat: gemini image generation integration

--- src/app/game/story/choose-story/choose-story.component.ts
+++ src/app/game/story/choose-story/choose-story.component.ts
@@ -1,13 +1,22 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, Signal, WritableSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  EventEmitter,
+  Output,
+  Signal,
+  WritableSignal
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { InputText } from 'primeng/inputtext';
 import { Story } from '../story.class';
 import { StoryService } from '../story.service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-story',
-  imports: [InputText, FormsModule, Button],
+  imports: [InputText, FormsModule, Button, NgOptimizedImage],
   templateUrl: './choose-story.component.html',
   standalone: true,
   styleUrl: './choose-story.component.scss',
@@ -20,6 +29,9 @@ export class ChooseStoryComponent {
 
   public premise: WritableSignal<string | undefined>;
 
+  public storyImg: Signal<any | undefined>;
+  public isStoryImgLoading: Signal<boolean>;
+
   @Output() storySelected = new EventEmitter<boolean>();
 
   constructor(private storyService: StoryService) {
@@ -27,6 +39,9 @@ export class ChooseStoryComponent {
     this.isLoading = this.storyService.isStoryLoading;
     this.isError = this.storyService.isStoryError;
     this.premise = this.storyService.premise;
+
+    this.storyImg = this.storyService.storyImg;
+    this.isStoryImgLoading = this.storyService.isStoryImgLoading;
   }
 
   public loadNewStory() {
